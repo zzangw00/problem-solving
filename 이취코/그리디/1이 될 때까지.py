49c8eeb778ffd2a2b10cd034ea63e1bd3d6dd27c
Chang Woo Shin <64726822+zzangw00@users.noreply.github.com>
2021-05-19 17:10:52 +0900

    그리디 algorithm

---
+++ 이취코/그리디/1이 될 때까지.py
@@ -0,0 +1,14 @@
+n, k = map(int, input().split())
+count = 0
+while True:
+    if n == 1:
+        break
+    else:
+        if n % k == 0:
+            n = n / k
+            count += 1
+        else:
+            n = n - 1
+            count += 1
+
+print(count)
