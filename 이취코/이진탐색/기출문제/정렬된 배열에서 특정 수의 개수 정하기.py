e4ff539f4cb739d34b378b200ccfeb463e131cf8
Chang Woo Shin <64726822+zzangw00@users.noreply.github.com>
2021-05-28 18:28:34 +0900

    이진탐색 algorithm 기출문제

---
+++ 이취코/이진탐색/기출문제/정렬된 배열에서 특정 수의 개수 정하기.py
@@ -0,0 +1,34 @@
+n, x = map(int, input().split())
+arr = list(map(int, input().split()))
+
+
+def first(arr, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if (mid == 0 or target > arr[mid - 1]) and arr[mid] == target:
+            return mid
+        if arr[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+
+def last(arr, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if (mid == n - 1 or target < arr[mid + 1]) and arr[mid] == target:
+            return mid
+        if arr[mid] <= target:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return -1
+
+
+a = first(arr, x, 0, n - 1)
+b = last(arr, x, 0, n - 1)
+
+if first(arr, x, 0, n - 1) != -1:
+    print(b - a + 1)
+else:
+    print(-1)
