f4b53062cee667f37f3c135cae0eb9487749f61b
Chang Woo Shin <64726822+zzangw00@users.noreply.github.com>
2021-05-31 21:01:17 +0900

    dp algorithm

---
+++ 이취코/DP/1로 만들기.py
@@ -0,0 +1,13 @@
+x = int(input())
+dp = []
+for i in range(x + 1):
+    dp.append(0)
+for i in range(2, x + 1):
+    dp[i] = dp[i - 1] + 1
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i // 2] + 1)
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i // 3] + 1)
+    if i % 5 == 0:
+        dp[i] = min(dp[i], dp[i // 5] + 1)
+print(dp[x])
