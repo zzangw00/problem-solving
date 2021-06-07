01c04a69fa05c4379ce59fc828f2732edb614fdd
Chang Woo Shin <64726822+zzangw00@users.noreply.github.com>
2021-06-07 18:08:58 +0900

    Add: 2021-06-07

---
+++ BOJ/DP/15988-1,2,3 더하기 3.py
@@ -0,0 +1,14 @@
+n = int(input())
+arr = []
+dp = [0] * 1000001
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+for i in range(n):
+    arr.append(int(input()))
+
+for i in range(4, 1000001):
+    dp[i] = (dp[i - 3] + dp[i - 2] + dp[i - 1]) % 1000000009
+
+for i in arr:
+    print(dp[i])
