f7a528d10b31ab3867a3d0937d8bd0c3747556e6
Chang Woo Shin <64726822+zzangw00@users.noreply.github.com>
2021-07-07 16:52:26 +0900

    Modify: 2021-07-07

--- BOJ/DP/11055-가장 큰 증가 부분 수열.py
+++ BOJ/DP/11055-가장 큰 증가 부분 수열.py
@@ -3,9 +3,10 @@ arr = list(map(int, input().split()))
 dp = []
 for i in arr:
     dp.append(i)
+
 for i in range(n):
     for j in range(i):
         if arr[i] > arr[j]:
             dp[i] = max(dp[i], dp[j] + arr[i])
 
-print(dp)
+print(max(dp))
