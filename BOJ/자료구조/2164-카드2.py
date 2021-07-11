eb655dfe1eb170336a17b05f689bb0430090b471
Chang Woo Shin <64726822+zzangw00@users.noreply.github.com>
2021-07-11 17:45:35 +0900

    Add: 2021-07-11

---
+++ BOJ/자료구조/2164-카드2.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+n = int(input())
+queue = deque()
+
+for i in range(1, n + 1):
+    queue.append(i)
+
+while len(queue) > 1:
+    queue.popleft()
+    a = queue.popleft()
+    queue.append(a)
+
+for i in queue:
+    print(i)
