5f1837395c8859174bd7d5013c0f5f2aa500810f
Chang Woo Shin <64726822+zzangw00@users.noreply.github.com>
2021-07-14 16:42:20 +0900

    Add: 2021-07-14

---
+++ BOJ/구현/15686-치킨 배달.py
@@ -0,0 +1,30 @@
+import itertools
+
+n, m = map(int, input().split())
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+
+chickenList = []
+houseList = []
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 2:
+            chickenList.append((i, j))
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1:
+            houseList.append((i, j))
+
+result = 100
+for i in range(1, m + 1):
+    combination = list(itertools.combinations(chickenList, i))
+    for c in combination:
+        dist = 0
+        for h in houseList:
+            minDist = min([abs(p[0] - h[0]) + abs(p[1] - h[1]) for p in c])
+            dist += minDist
+        if dist < result:
+            result = dist
+
+print(result)
