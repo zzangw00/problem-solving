16ed36a2d9314cf63f6c595ff47dde91e3b3565a
Chang Woo Shin <64726822+zzangw00@users.noreply.github.com>
2021-10-02 14:47:43 +0900

    Add: 2021-10-02

---
+++ 프로그래머스/1단계/소수 찾기.py
@@ -0,0 +1,19 @@
+import math
+
+
+def isPrimeNum(n):
+    for i in range(2, int(math.sqrt(n))+1):
+        if n % i == 0:
+            return False
+
+    return True
+
+
+def solution(n):
+    answer = 0
+    for i in range(2, n + 1):
+        a = isPrimeNum(i)
+        if a == True:
+            answer += 1
+
+    return answer
