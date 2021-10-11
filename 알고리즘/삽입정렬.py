235b2c76d22fe7b6e860388277a6d1d74174bd6b
Chang Woo Shin <64726822+zzangw00@users.noreply.github.com>
2021-10-11 19:20:07 +0900

    Modify: 2021-10-11

--- 알고리즘/삽입정렬.py
+++ 알고리즘/삽입정렬.py
@@ -1,8 +1,8 @@
-arr = [4, 3, 1, 5]
+arr = [4, 3, 1, 2]
 
 for end in range(1, len(arr)):
     for i in range(end, 0, -1):
-        if arr[i - 1] > arr[i]:
+        if arr[i - 1] < arr[i]:
             arr[i - 1], arr[i] = arr[i], arr[i - 1]
 
 print(arr)
