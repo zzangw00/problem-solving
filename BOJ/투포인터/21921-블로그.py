2758479a42955915eec1042aed42dcb40ecd09e7
Chang Woo Shin <64726822+zzangw00@users.noreply.github.com>
2022-03-25 19:19:24 +0900

    Add: 2022-03-25

---
+++ BOJ/투포인터/21921-블로그.py
@@ -0,0 +1,21 @@
+n, x = map(int, input().split())
+arr = list(map(int, input().split()))
+dp = [0] * (len(arr) + 1)
+answer = 0
+cnt = 1
+for i in range(1, len(dp)):
+    dp[i] = dp[i - 1] + arr[i - 1]
+for i in range(n - x + 1):
+    end = i + x - 1
+    result = dp[end + 1] - dp[i]
+    print(result)
+    if result > answer:
+        answer = result
+        cnt = 1
+    elif answer == result:
+        cnt += 1
+if answer == 0:
+    print('SAD')
+else:
+    print(answer)
+    print(cnt)
