1f4b8518dbe913bc588b9d81e1def850c6d032d4
Chang Woo Shin <64726822+zzangw00@users.noreply.github.com>
2022-03-25 17:46:56 +0900

    Add: 2022-03-25

---
+++ BOJ/투포인터/11728-배열 합치기.py
@@ -0,0 +1,25 @@
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+x = 0
+y = 0
+answer = []
+while True:
+    if x == n or y == m:
+        break
+    if a[x] > b[y]:
+        answer.append(b[y])
+        y += 1
+    else:
+        answer.append(a[x])
+        x += 1
+print(answer)
+if x != n:
+    for i in range(x, n):
+        answer.append(a[i])
+else:
+    for i in range(y, m):
+        answer.append(b[i])
+
+print(' '.join(map(str, answer)))
