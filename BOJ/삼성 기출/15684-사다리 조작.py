fe289bb3834a5a0358d97c70f8c7356dfbe0f2cc
Chang Woo Shin <64726822+zzangw00@users.noreply.github.com>
2022-04-25 23:53:43 +0900

    Add: 2022-04-25

--- BOJ/삼성 기출/15684-사다리 조작.py
+++ BOJ/삼성 기출/15684-사다리 조작.py
@@ -1,58 +1,99 @@
+import copy
+
 n, m, h = map(int, input().split())
-arr = []
-for _ in range(m):
-    arr.append(list(map(int, input().split())))
 graph = []
 for _ in range(h):
     graph.append([0] * n)
-for i in range(len(arr)):
-    a, b = arr[i]
+for _ in range(m):
+    a, b = map(int, input().split())
     graph[a - 1][b - 1] = 1
-
+temp = []
 answer = 4
 
 
 def solution():
-    dfs(0, 0, 0)
-
-    return answer
-
-
-def find():
-    for i in range(len(graph[0])):
-        k = i
-        for j in range(len(graph)):
-            if graph[j][k] == 1:
-                k += 1
-            elif k > 0 and graph[j][k - 1] == 1:
-                k -= 1
-        if i != k:
-            return False
-    return True
+    if m == 0:
+        return 0
+    for i in range(4):
+        dfs(0, i)
+    if answer == 1e9:
+        return -1
+    else:
+        return answer
 
 
-def dfs(cnt, x, y):
+def dfs(a, l):
+    if len(temp) > answer:
+        return
+    if len(temp) == l:
+        check(temp, l)
+        return
+    for i in range(a, h):
+        for j in range(n - 1):
+            if graph[i][j] == 0:
+                if temp:
+                    if j > 0 and temp[-1][0] == i and temp[-1][1] == j - 1:
+                        continue
+                    else:
+                        if (j > 0 and graph[i][j - 1] == 1) or graph[i][j + 1] == 1:
+                            continue
+                        else:
+                            temp.append([i, j])
+                            dfs(i + 1, l)
+                            temp.pop()
+                else:
+                    if (j > 0 and graph[i][j - 1] == 1) or graph[i][j + 1] == 1:
+                        continue
+                    else:
+                        temp.append([i, j])
+                        dfs(i + 1, l)
+                        temp.pop()
+
+
+def check(temp, l):
     global answer
-    if find():
-        answer = min(answer, cnt)
-        return
-    elif cnt == 3 or cnt >= answer:
-        return
-    for i in range(x, len(graph)):
-        if i == a:
-            k = y
-        else:
-            k = 0
-        for j in range(k, len(graph[0]) - 1):
-            if not graph[i][j] and not graph[i][j + 1]:
-                if j > 0 and graph[i][j - 1]:
-                    continue
-                graph[i][j] = 1
-                dfs(cnt + 1, i, j + 2)
-                graph[i][j] = 0
+    cnt = 0
+    visited = copy.deepcopy(graph)
+    for i in range(len(temp)):
+        x, y = temp[i]
+        visited[x][y] = 1
+    dx = [0, 0, 1]
+    dy = [-1, 1, 0]
+    for i in range(n):
+        x = 0
+        y = i
+        while True:
+            if visited[x][y] == 1:
+                d = 1
+                x += dx[d]
+                y += dy[d]
+                d = 2
+                x += dx[d]
+                y += dy[d]
+            elif y >= 1:
+                if visited[x][y - 1] == 1:
+                    d = 0
+                    x += dx[d]
+                    y += dy[d]
+                    d = 2
+                    x += dx[d]
+                    y += dy[d]
+                else:
+                    d = 2
+                    x += dx[d]
+                    y += dy[d]
+            else:
+                d = 2
+                x += dx[d]
+                y += dy[d]
+            if x == h:
+                if y == i:
+                    cnt += 1
+                    break
+                else:
+                    break
+    if cnt == n:
+        answer = min(answer, l)
 
 
-result = solution()
-if result >= 4:
-    result = -1
-print(result)
+print(solution())
