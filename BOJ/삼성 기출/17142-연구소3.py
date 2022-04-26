315cd5497c8b8d7d6db684075077b29337d30d54
Chang Woo Shin <64726822+zzangw00@users.noreply.github.com>
2022-04-26 13:45:26 +0900

    Modify: 2022-04-26

--- BOJ/삼성 기출/17142-연구소3.py
+++ BOJ/삼성 기출/17142-연구소3.py
@@ -1,99 +1,79 @@
-from itertools import combinations
 from collections import deque
-import copy
 
 n, m = map(int, input().split())
 graph = []
 for _ in range(n):
     graph.append(list(map(int, input().split())))
-b = []
+h = []
 for x in range(n):
     for y in range(n):
         if graph[x][y] == 2:
-            b.append([x, y])
-virus = list(combinations(b, m))
+            h.append([x, y])
+visited = [0] * len(h)
 temp = []
-aCnt = 0
-for _ in range(n):
-    temp.append([0] * n)
-for x in range(n):
-    for y in range(n):
-        if graph[x][y] == 2:
-            temp[x][y] = '*'
-        elif graph[x][y] == 1:
-            temp[x][y] = '-'
-        else:
-            aCnt += 1
-            temp[x][y] = 0
+answer = 1e9
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 
 def solution():
-    answer = 1e9
-    for i in range(len(virus)):
-        arr = copy.deepcopy(temp)
-        for j in range(len(virus[i])):
-            x, y = virus[i][j]
-            arr[x][y] = -1
-        a = bfs(arr, virus[i])
-        answer = min(answer, a)
-    if answer == 1e9:
-        answer = -1
+    k = 0
+    for x in range(n):
+        for y in range(n):
+            if graph[x][y] == 0:
+                k += 1
+    if k == 0:
+        return 0
+    dfs(0)
     return answer
 
 
-def bfs(arr, virus):
+def dfs(a):
+    if len(temp) == m:
+        bfs(temp)
+        return
+    for i in range(a, len(h)):
+        if visited[i] == 0:
+            visited[i] = 1
+            temp.append(h[i])
+            dfs(i + 1)
+            temp.pop()
+            visited[i] = 0
+
+
+def bfs(temp):
+    global answer
+    cnt = 0
+    num = 0
     visited = []
-    result = 0
     for _ in range(n):
         visited.append([0] * n)
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    queue = deque(virus)
-    time = 1
-    cnt = len(queue)
-    pushCnt = 0
-    p = 0
-    aaa = 0
-    ax = 0
-    for x in range(n):
-        for y in range(n):
-            if arr[x][y] == 0:
-                p += 1
-    if p == 0:
-        return 0
+    queue = deque(temp)
+    for i in range(len(queue)):
+        x, y = queue[i]
+        visited[x][y] = 1
     while queue:
         x, y = queue.popleft()
-        cnt -= 1
-        visited[x][y] = 1
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if nx >= n or nx < 0 or ny >= n or ny < 0 or visited[nx][ny] == 1 or arr[nx][ny] == '-' or arr[nx][ny] == -1:
+            if nx >= n or nx < 0 or ny >= n or ny < 0 or visited[nx][ny] != 0 or graph[nx][ny] == 1:
                 continue
             else:
-                if arr[nx][ny] == 0:
-                    aaa += 1
-                arr[nx][ny] = time
-                pushCnt += 1
                 queue.append([nx, ny])
-                visited[nx][ny] = 1
-                if aaa == aCnt:
-                    ax += 1
-                    break
-        if ax == 1:
-            break
-        if cnt == 0:
-            time += 1
-            cnt = pushCnt
-            pushCnt = 0
+                visited[nx][ny] = visited[x][y] + 1
     for x in range(n):
         for y in range(n):
-            if arr[x][y] == 0:
-                return 1e9
-            z = str(arr[x][y])
-            if z.isdigit() == True:
-                result = max(result, arr[x][y])
-    return result
+            if graph[x][y] != 2:
+                num = max(num, visited[x][y])
+            if visited[x][y] == 0 and graph[x][y] == 0:
+                cnt += 1
+    if cnt == 0:
+        answer = min(answer, num - 1)
 
 
-print(solution())
+result = solution()
+if result == 1e9:
+    print(-1)
+else:
+    print(result)
