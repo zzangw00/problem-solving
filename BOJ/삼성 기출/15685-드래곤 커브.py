fcbe81c330e1c940f925c3e0a6625ecdce2eb16e
Chang Woo Shin <64726822+zzangw00@users.noreply.github.com>
2022-04-26 13:09:06 +0900

    Modify: 2022-04-26

--- BOJ/삼성 기출/15685-드래곤 커브.py
+++ BOJ/삼성 기출/15685-드래곤 커브.py
@@ -1,39 +1,43 @@
 n = int(input())
-arr = []
+visited = []
+for _ in range(100):
+    visited.append([0] * 100)
+dragon = []
 for _ in range(n):
-    arr.append(list(map(int, input().split())))
+    dragon.append(list(map(int, input().split())))
+
+dx = [0, -1, 0, 1]
+dy = [1, 0, -1, 0]
 
 
-def solution(arr):
-    answer = 0
-    visited = [[0] * 101 for _ in range(101)]
-    dx = [1, 0, -1, 0]  # 0, 1, 2, 3
-    dy = [0, -1, 0, 1]
-    curve = [[] for _ in range(len(arr))]
-    for i in range(len(arr)):
-        x, y, d, g = arr[i]
-        curve[i].append(d)
-        for _ in range(g):
-            reverse = list(reversed(curve[i]))
-            for j in reverse:
-                if j + 1 == 4:
-                    curve[i].append(0)
-                else:
-                    curve[i].append(j + 1)
-    for i in range(len(arr)):
-        x, y, d, g = arr[i]
-        visited[y][x] = 1
-        for j in curve[i]:
-            x = x + dx[j]
-            y = y + dy[j]
-            if 0 <= x <= 100 and 0 <= y <= 100:
-                visited[y][x] = 1
-    for x in range(100):
-        for y in range(100):
-            if visited[x][y] == 1 and visited[x + 1][y] == 1 and visited[x + 1][y + 1] == 1 and visited[x][y + 1] == 1:
-                answer += 1
-
-    return answer
+def solution():
+    for i in range(n):
+        x, y, d, g = dragon[i]
+        move = []
+        move.append(d)
+        for i in range(g):
+            temp = []
+            for j in range(len(move) - 1, -1, -1):
+                temp.append((move[j] + 1) % 4)
+            move += temp
+        check(x, y, move)
 
 
-print(solution(arr))
+def check(x, y, move):
+    global visited
+    visited[x][y] = 1
+    for i in range(len(move)):
+        d = move[i]
+        x += dx[d]
+        y += dy[d]
+        visited[x][y] = 1
+
+
+answer = 0
+
+solution()
+for x in range(99):
+    for y in range(99):
+        if visited[x][y] == 1 and visited[x][y + 1] == 1 and visited[x + 1][y + 1] == 1 and visited[x + 1][y] == 1:
+            answer += 1
+print(answer)
